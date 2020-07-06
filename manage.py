88daf8a2c279d7f3ca23d2b3eb15a56361a5f047
Yaroslav Chykhut <yaroslav.chykhut@gmail.com>
2020-07-06 15:21:40 +0300

    Fixed release archive filename

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ from app.lucidum import update_lucidum, AppError
 
 if __name__ == "__main__":
     try:
-        update_lucidum('release-0.0.1.tar.gz1')
+        update_lucidum('release-0.0.1.tar.gz')
     except AppError as e:
         logger.exception(e)
     except Exception as e:

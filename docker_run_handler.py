9930fd94a7656f0185e44c5d88bc683ad1841c6f
Yaroslav Chykhut <yaroslav.chykhut@gmail.com>
2020-08-21 14:13:17 +0300

    Fixed docker-run command to work with local images, added more logging

--- docker_run_handler.py
+++ docker_run_handler.py
@@ -2,32 +2,35 @@ import sys
 
 from loguru import logger
 
-from config_handler import get_ecr_images, docker_client
+from config_handler import docker_client, get_local_images
 from exceptions import AppError
 
 NETWORK = "lucidum_default"
 
 
 def run_command(image_data: dict, cmd: str):
-    kwargs = {}
-    if cmd:
-        kwargs["command"] = cmd
+    image_name = f"{image_data['name']}:{image_data['version']}"
+    kwargs, message = {}, [f"Docker image: {image_name}"]
     host_path, docker_path = image_data.get("hostPath"), image_data.get("dockerPath")
     if host_path and docker_path:
+        message += [f"Host path: {host_path}", f"Docker path: {docker_path}"]
         kwargs["volumes"] = {
             host_path: {
                 "bind": docker_path,
                 "mode": "rw"
             }
         }
-    docker_client.containers.run(
-        f"{image_data['name']}:{image_data['version']}", remove=True, network=NETWORK, **kwargs
-    )
+    message.append(f"Network: {NETWORK}")
+    logger.info("Docker run parameters:\n{}", "\n".join(message))
+    docker_client.containers.run(image_name, remove=True, network=NETWORK, command=cmd, **kwargs)
 
 
 @logger.catch(onerror=lambda _: sys.exit(1))
 def run(component: str, cmd: str):
-    image_data = next((image for image in get_ecr_images() if f"{image['name']}:{image['version']}" == component), None)
+    image_data = next(
+        (image for image in get_local_images() if f"{image['name']}:{image['version']}" == component), None
+    )
     if not image_data:
         raise AppError(f"Component '{component}' was not found in image list")
+    logger.info("Running command within '{}' component: {}", component, cmd)
     run_command(image_data, cmd)

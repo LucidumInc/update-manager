1ae60f726caad6d3d06ac0988647d6bc7739dfd5
nando <nando@hex7.com>
2021-04-14 16:42:50 -0400

    fixup docker tests

--- test/post-deploy/features/steps/docker_tests.py
+++ test/post-deploy/features/steps/docker_tests.py
@@ -1,23 +1,44 @@
 from behave import *
 import docker
+import yaml
+import os
 
 client = docker.from_env()
 
 
-@given('docker image "{docker_image}" is local')
-def step_impl(context, docker_image):
-  for image in client.images.list():
-    if docker_image in str(image):
-      break
+@given('docker-compose config "{docker_compose_config}" is present')
+def step_impl(context, docker_compose_config):
+  context.docker_containers = []
+
+  if os.path.isfile(docker_compose_config):
+    with open(docker_compose_config, 'r') as yfile:
+      dconf = yaml.safe_load(yfile)
+    for key, value in dconf['services'].items():
+      context.docker_containers.append(value['image'])
+
   else:
     assert context.failed is True
 
 
-@then('docker containers should be running')
+@then('docker-compose images should be local')
 def step_impl(context):
-  num_containers = len(client.containers.list(all=True))
+  docker_images = []
 
-  # NOTE: parse docker-compose.yml and test each container is running
-  #       rather than just testing number of containers
-  if num_containers < 4:
-    assert context.failed is True
+  for local_image in client.images.list():
+    docker_images.append(local_image.tags[0])
+
+  for compose_container in context.docker_containers:
+    if compose_container not in docker_images:
+      assert context.failed is True
+
+
+@then('docker-compose containers should be running')
+def step_impl(context):
+  local_containers = []
+
+  for container in client.containers.list():
+    local_containers.append(container.image.tags[0])
+
+  for compose_container  in context.docker_containers:
+    if compose_container not in local_containers:
+      assert context.failed is True

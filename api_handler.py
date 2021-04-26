48f83ba2a4f1571a2cefbbf2d1ceae24bfca659b
Yaroslav Chykhut <yaroch137@gmail.com>
2021-04-26 15:25:38 +0300

    Added logging configuration for api_handler

--- api_handler.py
+++ api_handler.py
@@ -1,9 +1,12 @@
 import os
+import sys
+import logging
 from typing import Optional
 
 import uvicorn
 from fastapi import FastAPI, APIRouter, HTTPException
 from jinja2 import Environment, BaseLoader
+from loguru import logger
 from pydantic import BaseModel
 
 from config_handler import get_lucidum_dir
@@ -11,6 +14,33 @@ from config_handler import get_lucidum_dir
 AIRFLOW_DOCKER_FILENAME = "airflow_docker.py"
 
 router = APIRouter(prefix="/update-manager/api")
+
+
+class InterceptHandler(logging.Handler):
+
+    def emit(self, record: logging.LogRecord) -> None:
+        try:
+            level = logger.level(record.levelname).name
+        except AttributeError:
+            level = record.levelno
+
+        frame, depth = logging.currentframe(), 2
+        while frame.f_code.co_filename == logging.__file__:
+            frame = frame.f_back
+            depth += 1
+
+        logger.opt(depth=depth, exception=record.exc_info).log(level, record.getMessage())
+
+
+def setup_logging():
+    logger.remove()
+    logger.add(sys.stdout, enqueue=True)
+    logger.add("logs/api_{time}.log", rotation="1 day", retention="30 days", diagnose=True)
+
+    logging.basicConfig(handlers=[InterceptHandler()], level=0)
+    for log_ in ["uvicorn", "uvicorn.error", "uvicorn.access", "fastapi"]:
+        logger_ = logging.getLogger(log_)
+        logger_.handlers = [InterceptHandler()]
 
 
 class AirflowModel(BaseModel):
@@ -62,6 +92,12 @@ def create_app() -> FastAPI:
 
 
 app = create_app()
+
+
+@app.on_event("startup")
+def startup_event():
+    setup_logging()
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0")

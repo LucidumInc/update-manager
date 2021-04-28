933ef7a06a993c95a26a753c00ca0452d22f9a33
Yaroslav Chykhut <yaroch137@gmail.com>
2021-04-28 17:47:57 +0300

    Fixed gunicorn configuration for number of workers

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,11 +1,7 @@
 import multiprocessing
 
 use_bind = "0.0.0.0:8000"
-
-cores = multiprocessing.cpu_count()
-workers_per_core = 1
-default_web_concurrency = workers_per_core * cores
-web_concurrency = max(int(default_web_concurrency), 2)
+web_concurrency = min(multiprocessing.cpu_count(), 2)
 
 # Gunicorn config variables
 bind = use_bind

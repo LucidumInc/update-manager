b928daf1378bfb1e2f2e5f4f8e3e3649b21f5089
Yaroslav Chykhut <yaroch137@gmail.com>
2021-06-03 12:17:04 +0300

    Added ssh tunnels healthcheck api

--- healthcheck_handler.py
+++ healthcheck_handler.py
@@ -127,6 +127,30 @@ def check_cron_health() -> dict:
     return result
 
 
+def get_systemctl_service_status(service_name: str, super_user: bool = False):
+    command = f"{'sudo ' if super_user else ''}systemctl is-active {service_name}"
+    result = subprocess.run(command.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    return result.stdout.decode().strip()
+
+
+def get_journalctl_service_logs(service_name: str):
+    command = f"journalctl -u {service_name} -n 50"
+    result = subprocess.run(command.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    return result.stdout.decode().strip().splitlines()
+
+
+def get_journalctl_service_pid(service_name: str):
+    command = f"systemctl show --property MainPID --value {service_name}"
+    result = subprocess.run(command.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    return int(result.stdout.decode().strip())
+
+
+def get_system_process_by_pid(pid: int):
+    command = f"ps -p {pid} -fww"
+    result = subprocess.run(command.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    return result.stdout.decode().strip().splitlines()
+
+
 class BaseInfoCollector:
     name = None
 
@@ -278,6 +302,31 @@ class ECRComponentsInfoCollector(BaseInfoCollector):
         if system_delta > 0.0:
             cpu_percent = cpu_delta / system_delta * 100.0 * cpu_count
         return cpu_percent
+
+
+class SSHTunnelsInfoCollector(BaseInfoCollector):
+    name = "ssh_tunnels"
+
+    def __call__(self):
+        result = {"status": "OK"}
+        statuses = []
+        services = ["lucidum-jumpbox-primary", "lucidum-jumpbox-secondary"]
+        for service in services:
+            pid = get_journalctl_service_pid(service)
+            status = get_systemctl_service_status(service)
+            service_status = {
+                "status": status,
+                "logs": get_journalctl_service_logs(service),
+                "process": get_system_process_by_pid(pid),
+            }
+            statuses.append(status)
+            result[service] = service_status
+
+        if all(status == "inactive" for status in statuses):
+            result["status"] = "FAILED"
+            result["message"] = "SSH tunnels are inactive"
+
+        return result
 
 
 def _build_health_info_obj(collector, result: dict = None):

b7892ecb6cfafefacdd713a0b779eeed12e663a8
lucidumio <sw@lucidum.io>
2024-11-04 09:10:22 -0800

    Update update manager connector test

--- helper.py
+++ helper.py
@@ -12,6 +12,7 @@ connector_table = db_client.client[db_client._mongo_db][connector_table_name]
 connector_test_result_table = db_client.client[db_client._mongo_db]['connector_test_result']
 action_config_table = db_client.client[db_client._mongo_db]['local_integration_configuration']
 
+
 def get_all_connector_profiles():
     result = []
     for item in connector_table.find():
@@ -26,8 +27,9 @@ def get_all_connector_profiles():
                            'profile_enabled': item.get('active', False),
                            'bridge_name': item['bridge_name'],
                            'bridge_display_name': item.get('display_name', item['bridge_name'])
-                          })
+                           })
     return result
+
 
 def run_connector_profile_test():
     result = get_all_connector_profiles()
@@ -35,7 +37,7 @@ def run_connector_profile_test():
         if record['profile_enabled'] and record['service_enabled']:
             logger.info(f"test connector: {record}")
             trace_id = uuid.uuid4().hex
-            url = f"http://localhost:8000/update-manager/api/connector/{record['connector_name']}/test/{record['bridge_name']}?profile_db_id={record['profile_id']}&trace_id={trace_id}";
+            url = f"http://localhost:8000/update-manager/api/connector/{record['connector_name']}/test/{record['bridge_name']}?profile_db_id={record['profile_id']}&trace_id={trace_id}"
             resp = requests.get(url)
             collection = connector_test_result_table
             test_results = collection.find_one({'trace_id': trace_id})
@@ -47,14 +49,16 @@ def run_connector_profile_test():
             can_update = False
             for test_result in test_results['test_result']:
                 for service in connector_config['services_list']:
-                    if test_result['name'] == service['service'] and test_result['status'] == 'FAIL':
+                    if test_result['name'] == service['service']:
                         service['status'] = test_result['status']
                         service['message'] = test_result['message']
                         can_update = True
             if can_update:
-                collection.update_one({"_id": ObjectId(test_results['profile_db_id'])}, {"$set": {"services_list": connector_config['services_list'], "last_tested_at": datetime.now()}})
+                collection.update_one({"_id": ObjectId(test_results['profile_db_id'])}, {
+                    "$set": {"services_list": connector_config['services_list'], "last_tested_at": datetime.now()}})
                 logger.info(f"DB test result updated.")
             logger.info(">>> testing connector profile Done.")
+
 
 def get_all_action_configs():
     results = []
@@ -62,6 +66,7 @@ def get_all_action_configs():
     for item in collection.find({}):
         results.append({"bridge_name": item['bridge_name'], "config_name": item["config_name"], "_id": item["_id"]})
     return results
+
 
 def run_action_config_test():
     logger.info(">>> testing action config start...")
@@ -73,7 +78,8 @@ def run_action_config_test():
         try:
             resp = requests.get(url)
             logger.info(f"test action config result: {resp.text}")
-            collection.update_one({"_id": result["_id"]}, {"$set": {"test_status": resp.json(), "last_tested_at": datetime.now()}})
+            collection.update_one({"_id": result["_id"]},
+                                  {"$set": {"test_status": resp.json(), "last_tested_at": datetime.now()}})
         except Exception as e:
             logger.warning(f"{url} error {e}")
     logger.info(">>> testing action config Done.")

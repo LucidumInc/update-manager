f30db1e7342ab2a1c9d49d80135cb9b1982e7595
Mike Giles <mike.giles@lucidumsecurity.com>
2025-05-14 09:20:55 -0400

    chore(DE-228): Removing legacy tomcat configuration files

--- init_handler.py
+++ init_handler.py
@@ -70,9 +70,7 @@ def create_web_directory(base_dir):
     copy_file(os.path.join("resources", "server.pem"), os.path.join(hostdata_dir, "server.pem"))
     copy_file(os.path.join("resources", "server_private.pem"), os.path.join(hostdata_dir, "server_private.pem"))
     copy_file(os.path.join("resources", "server.xml"), os.path.join(conf_dir, "server.xml"))
-    copy_file(os.path.join("resources", "web.xml"), os.path.join(conf_dir, "web.xml"))
     copy_file(os.path.join("resources", "index.jsp"), os.path.join(app_dir, "index.jsp"))
-    copy_file(os.path.join("resources", "context.xml"), os.path.join(conf_dir, "context.xml"))
     if created:
         change_permissions_recursive(web_dir, 0o777)
     return web_dir
